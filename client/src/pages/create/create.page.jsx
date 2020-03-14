a7fb8fd9ad15a010ab6599d12e7b29a31efd341c
Jake <jake@Emilys-MBP.lan>
2020-03-13 21:50:58 -0700

    framing for inputs started

--- client/src/pages/create/create.page.jsx
+++ client/src/pages/create/create.page.jsx
@@ -1,31 +1,52 @@
 import React, {useState} from 'react';
-import {Container, Paper, Grid, Card} from '@material-ui/core';
-import { useForm } from "react-hook-form";
+import {Container, Paper, Grid, TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { useForm, Controller } from "react-hook-form";
+import "./create.page.css"
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: 200,
+      },
+    },
+
+  }));
+
+const InputComponent = ({name, label, control}) => {
+    return (
+        <Grid item xs={12} sm={6} xl={4}>
+            <Controller as={TextField} fullWidth variant="outlined" label={name} name={name} control={control} defaultValue="" />
+        </Grid>
+    )
+}
 
 const CreatePage = () => {
-    const { handleSubmit, register, errors } = useForm();
+    const { handleSubmit, control, register, errors } = useForm();
     const onSubmit = values => {
       console.log(values);
     };
+    const classes = useStyles();
 
     return (
         <Paper>
             <Grid container spacing={3}>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                <input name="name" />
-                <input name="links" />
-                <input name="summary" type="number" />
-                <input name="whatItConnectsTo" />
-                <input name="bigIdea" />
-                <input name="whatINeedItFor" />
-                <input name="simpleExplanation" />
-                <input name="whenIWouldUse" />
-                <input name="furtherQuestions" />
-                <input name="example" />
-                <input name="howToRecall" />
-                <input name="itsLike" type="submit" />
-                
-                    <button type="submit">Submit</button>
+                <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+                <div className="createLinkForm__inputContainer">
+                <InputComponent control={control} name="name"/>
+                <InputComponent control={control} name="summary" />
+                <InputComponent control={control} name="whatItConnectsTo" />
+                <InputComponent control={control} name="bigIdea" />
+                <InputComponent control={control} name="whatINeedItFor" />
+                <InputComponent control={control} name="simpleExplanation" />
+                <InputComponent control={control} name="whenIWouldUse" />
+                <InputComponent control={control} name="furtherQuestions" />
+                <InputComponent control={control} name="example" />
+                <InputComponent control={control} name="howToRecall" />
+                <InputComponent control={control} name="itsLike" />
+                </div>
+                <button type="submit">Submit</button>
                 </form>
             </Grid>
         </Paper>
